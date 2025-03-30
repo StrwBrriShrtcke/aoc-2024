0e6444999bcaf3194e7793adfc5b44b508afc5df
Jia Wei <leejiawei241@gmail.com>
2025-03-30 08:34:14 +0800

    Used Map instead of object (๑'ᵕ'๑)⸝*

--- day1/p2.js
+++ day1/p2.js
@@ -1,15 +1,17 @@
 import { rL } from "./p1.js"
 import { lL } from "./p1.js"
 
-const rLObject = {};
+const rLMap = new Map();
 
 for (const element of rL) {
-  rLObject[element] = 1 + (rLObject[element] ?? 0)
+  rLMap.set(element, 1 + (rLMap.get(element) ?? 0));
 }
 
 let sum = 0
 
 for (const element of lL) {
-  sum += element * (rLObject[element] ?? 0)
+  if (rLMap.get(element)) {
+    sum += element * rLMap.get(element);
+  }
 }
 console.log(sum)
